670a53e3a8ccaee585ca9e1c4aee66917ca44f71
Siwakorn Pasawang <siwakorn.pas@ku.th>
2023-08-07 12:38:24 +0700

    Create Service and Controller for API

---
+++ src/main/java/ku/cs/restaurant/service/RestaurantService.java
@@ -0,0 +1,23 @@
+package ku.cs.restaurant.service;
+
+import ku.cs.restaurant.entity.Restaurant;
+import ku.cs.restaurant.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RestaurantService {
+    @Autowired
+    private RestaurantRepository repository;
+
+    public List<Restaurant> getAll(){
+        return repository.findAll();
+    }
+
+    public Restaurant create(Restaurant restaurant){
+        Restaurant record = repository.save(restaurant);
+        return record;
+    }
+}
